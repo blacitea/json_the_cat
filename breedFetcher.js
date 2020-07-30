b887410d2390c81519a360862d89c0ea017fdf77
Angel Tsang <blacitea@gmail.com>
2020-07-30 21:55:39 +0000

    use JSON.parse(), changed body string to obj

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,9 @@ const request = require("request");
 
 let breedInfo;
 
-request("https://api.thecatapi.com/v1/breeds/search?q=Siberian", (error, response, data) => {
+request("https://api.thecatapi.com/v1/breeds/search?q=Siberian", (error, response, body) => {
+  const data = JSON.parse(body);
+  console.log(data);
   console.log(typeof (data));
 });
 
