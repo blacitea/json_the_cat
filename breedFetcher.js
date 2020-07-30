a881bb7f107144d49d53dcd540037347d98ad8de
Angel Tsang <blacitea@gmail.com>
2020-07-30 21:54:03 +0000

    use type of to check data from request

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,9 @@
 const request = require("request");
 
+let breedInfo;
+
 request("https://api.thecatapi.com/v1/breeds/search?q=Siberian", (error, response, data) => {
-  console.log(data);
+  console.log(typeof (data));
 });
+
+//console.log(breedInfo);
