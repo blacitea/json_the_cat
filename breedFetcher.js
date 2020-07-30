d77889fc100c54e03114ef0b24ff007d582db79e
Angel Tsang <blacitea@gmail.com>
2020-07-30 22:04:51 +0000

    make write func that print 1st elem of the data array

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,8 +4,12 @@ let breedInfo;
 
 request("https://api.thecatapi.com/v1/breeds/search?q=Siberian", (error, response, body) => {
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof (data));
+  write(data);
 });
+
+const write = objData => {
+  console.log(objData[0]);
+};
+
 
 //console.log(breedInfo);
