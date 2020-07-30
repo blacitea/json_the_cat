447e45430dc2ad867e2e0dc4e3222f4e61037db7
Angel Tsang <blacitea@gmail.com>
2020-07-30 22:59:21 +0000

    refactor into fetchBreedDesc

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,26 +2,34 @@ const request = require("request");
 const input = process.argv[2];
 
 const host = "https://api.thecatapi.com/v1/breeds/search";
-const searchURL = host.concat("?q=", input);
 const noBreedError = "Sorry! We haven't encounter them yet! \nWe will continue our search\nInto the outer space .....🛸🛸🛸🛸";
 const invalidURLError = "Oops! The path to the cathouse was interupted by dogs!\n🐕  🐩  🐕  🐩\nFind and use a new path!\n 🐱 🐾 🐱 🐾 ";
 
-request(searchURL, (error, response, body) => {
-  if (error) {
-    console.log(invalidURLError);
-  } else {
-    const data = JSON.parse(body);
-    if (!data[0]) {
-      console.log(noBreedError);
-    } else {
-      write(data);
-    }
-  }
-});
 
-const write = objData => {
-  console.log(objData[0].description);
+const writeDesc = (err, data) => {
+  if (err) {
+    console.log(invalidURLError);
+    process.exit();
+  }
+  if (data[0]) {
+    console.log(data[0].description);
+  } else {
+    console.log(noBreedError);
+    process.exit();
+  }
 };
 
 
-//console.log(breedInfo);
+const fetchBreedDescription = function (breedName, callback) {
+  const searchURL = host.concat("?q=", breedName);
+  request(searchURL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data);
+    }
+  });
+};
+
+fetchBreedDescription(input, writeDesc);
