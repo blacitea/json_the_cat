3975cb88f222ae4e3f4078b6f26daacda4e9f5ef
Angel Tsang <blacitea@gmail.com>
2020-07-30 21:44:22 +0000

    retry api with query (?q=breedName)

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,5 @@
 const request = require("request");
 
-request("https://api.thecatapi.com/v1/breeds/search", (error, response, data) => {
+request("https://api.thecatapi.com/v1/breeds/search?q=Siberian", (error, response, data) => {
   console.log(data);
 });
