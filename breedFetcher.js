8c6168828c3c733764b6ebb763c2ea79d8bf6970
Angel Tsang <blacitea@gmail.com>
2020-07-30 22:40:18 +0000

    added error msg for invalid url

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,14 +3,19 @@ const input = process.argv[2];
 
 const host = "https://api.thecatapi.com/v1/breeds/search";
 const searchURL = host.concat("?q=", input);
-const noBreedError = "Sorry! We haven't encounter them yet! \nWe will continue our search\nInto the outer space .....🛸🛸🛸🛸"
+const noBreedError = "Sorry! We haven't encounter them yet! \nWe will continue our search\nInto the outer space .....🛸🛸🛸🛸";
+const invalidURLError = "Oops! The path to the cathouse was interupted by dogs!\n🐕  🐩  🐕  🐩\nFind and use a new path!\n 🐱 🐾 🐱 🐾 ";
 
 request(searchURL, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (!data[0]) {
-    console.log(noBreedError);
+  if (error) {
+    console.log(invalidURLError);
   } else {
-    write(data);
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      console.log(noBreedError);
+    } else {
+      write(data);
+    }
   }
 });
 
